2b45fffefdbad08fdb59e407865fa5f62755e479
Emily Casey <ecasey@pivotal.io>
2020-06-02 16:36:03 -0400

    Adds support for spring-ldap
    Resolves #12
    
    Signed-off-by: Emily Casey <ecasey@pivotal.io>

---
+++ src/test/java/org/springframework/cloud/bindings/boot/LDAPBindingsPropertiesProcessorTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.bindings.boot;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.cloud.bindings.Binding;
+import org.springframework.cloud.bindings.Bindings;
+import org.springframework.cloud.bindings.FluentMap;
+import org.springframework.mock.env.MockEnvironment;
+
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.cloud.bindings.boot.LDAPBindingsPropertiesProcessor.KIND;
+
+@DisplayName("LDAP BindingsPropertiesProcessor")
+final class LDAPBindingsPropertiesProcessorTest {
+
+    private final Bindings bindings = new Bindings(
+            new Binding("test-name", Paths.get("test-path"),
+                    Collections.singletonMap("kind", KIND),
+                    new FluentMap()
+                            .withEntry("urls", "test-urls")
+                            .withEntry("password", "test-password")
+                            .withEntry("username", "test-username")
+                            .withEntry("base", "test-base")
+            )
+    );
+
+    private final MockEnvironment environment = new MockEnvironment();
+
+    private final HashMap<String, Object> properties = new HashMap<>();
+
+    @Test
+    @DisplayName("contributes LDAP properties")
+    void testJdbc() {
+        new LDAPBindingsPropertiesProcessor().process(environment, bindings, properties);
+        assertThat(properties)
+                .containsEntry("spring.ldap.urls", "test-urls")
+                .containsEntry("spring.ldap.username", "test-username")
+                .containsEntry("spring.ldap.password", "test-password")
+                .containsEntry("spring.ldap.base", "test-base");
+    }
+
+    @Test
+    @DisplayName("can be disabled")
+    void disabled() {
+        environment.setProperty("org.springframework.cloud.bindings.boot.ldap.enable", "false");
+
+        new OracleBindingsPropertiesProcessor().process(environment, bindings, properties);
+
+        assertThat(properties).isEmpty();
+    }
+
+}
