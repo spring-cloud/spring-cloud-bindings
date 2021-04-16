cee2609022c0a31a377fd81fc14685bad7281841
Alexander Schwab <alexander.schwab@ti8m.ch>
2021-04-16 12:41:15 +0200

    Adding support for ActiveMQ Artemis

--- src/test/java/org/springframework/cloud/bindings/boot/BindingSpecificEnvironmentPostProcessorTest.java
+++ src/test/java/org/springframework/cloud/bindings/boot/BindingSpecificEnvironmentPostProcessorTest.java
@@ -104,7 +104,7 @@ final class BindingSpecificEnvironmentPostProcessorTest {
     @Test
     @DisplayName("included implementations are registered")
     void includedImplementations() {
-        assertThat(new BindingSpecificEnvironmentPostProcessor().processors).hasSize(19);
+        assertThat(new BindingSpecificEnvironmentPostProcessor().processors).hasSize(20);
     }
 
 }
