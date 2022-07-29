0d1baf616f22c0810fe2330a6a2e3826181dacb1
David O'Sullivan <davidos@vmware.com>
2022-07-29 13:44:26 +0100

    Adds support for promoting to maven central

---
+++ scripts/promote-to-maven-central.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export BUILD_INFO_LOCATION=$(pwd)/repository/build-info.json
+
+java -jar /opt/concourse-release-scripts.jar publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" repository
+
+echo "Sync complete"
