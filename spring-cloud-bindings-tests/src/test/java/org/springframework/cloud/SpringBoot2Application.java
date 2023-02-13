874b54b4b615ea060719599a4a7d6884599d9e8d
Anthony Dahanne <anthony.dahanne@gmail.com>
2023-02-13 14:25:29 -0500

    Bump to latest 2.5 spring boot release
    Contains feedback from @Kehrlann
    
    Original commit by @abelsromero - amended to keep compatibility with Spring Boot 2
    
    Add integration tests
    * use Maven modules to do so
    * spring-cloud-bindings goes one level down
    * addition of a spring-cloud-bindings parent
    
    Inspired by @gregturn and spring-data-rest
    
    Integrate PR#94 comments from @Kehrlann
    
    * move bindings to src/test/resources
    * get rid of unused / duplicate annotations
    * move all dependencies to test scope for test projects
    
    Spring Boot 2 support
    * only in branch 1.x, SB3 support is in main branch

---
+++ spring-cloud-bindings-tests/src/test/java/org/springframework/cloud/SpringBoot2Application.java
@@ -0,0 +1,7 @@
+package org.springframework.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2Application {
+}
--- spring-cloud-bindings/src/main/java/org/springframework/cloud/bindings/boot/BindingFlattenedEnvironmentPostProcessor.java
+++ spring-cloud-bindings/src/main/java/org/springframework/cloud/bindings/boot/BindingFlattenedEnvironmentPostProcessor.java
@@ -17,7 +17,7 @@
 package org.springframework.cloud.bindings.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor;
 import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.logging.DeferredLog;
@@ -59,7 +59,7 @@ public final class BindingFlattenedEnvironmentPostProcessor implements Applicati
     @Override
     public int getOrder() {
         // Before ConfigFileApplicationListener so values there can use values from {@link Bindings}.
-        return ConfigFileApplicationListener.DEFAULT_ORDER - 1;
+        return ConfigDataEnvironmentPostProcessor.ORDER - 1;
     }
 
     @Override
