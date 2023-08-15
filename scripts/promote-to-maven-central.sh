a77d20e10560c1e89913ea3a4b29922cfd855d1d
Anthony Dahanne <anthony.dahanne@gmail.com>
2023-08-15 15:57:17 -0400

    Adapt to CI * now using springio/concourse-release-scripts

--- scripts/promote-to-maven-central.sh
+++ scripts/promote-to-maven-central.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 export BUILD_INFO_LOCATION=$(pwd)/repository/build-info.json
 
-java -jar /opt/concourse-release-scripts.jar publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" repository
+java -jar /concourse-release-scripts.jar publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" repository
 
 echo "Sync complete"
